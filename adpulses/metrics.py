966e333d91f15aec5df4d5379067cad86f260c85
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2021-06-19 16:36:14 -0400

    On branch master 	modified:   metrics.py 	modified:   optimizers.py

--- adpulses/metrics.py
+++ adpulses/metrics.py
@@ -57,7 +57,7 @@ def err_ml2xy(Mr_: Tensor, Md_: Tensor, w_: Optional[Tensor] = None) -> Tensor:
     - `err` (1,)
     """
     lam1 = 1.0 #1.0
-    lam2 = 0.3 #0.3
+    lam2 = 2.0 #0.3
     Me_ = Mr_[..., :2].norm(dim=-1) - Md_[..., :2].norm(dim=-1)
     errmag = (Me_ if w_ is None else Me_*w_).norm()**2
     Me_ = (Mr_[..., :2] - Md_[..., :2])
