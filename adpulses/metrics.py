3d6c2f5d47185b56b7b183d3785aaa7d1a64d7bc
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2022-11-03 20:33:09 -0400

    On branch master 	modified:   metrics.py

--- adpulses/metrics.py
+++ adpulses/metrics.py
@@ -58,7 +58,7 @@ def err_ml2xy(Mr_: Tensor, Md_: Tensor, w_: Optional[Tensor] = None) -> Tensor:
     - `err` (1,)
     """
     lam1 = 1e2 # 12/6/21: 1.0.  # mxy magnitude error weighting (enforce limits)
-    lam2 = 1e-2 # 12/6/21: 2.0   # mxy complex error weighting
+    lam2 = 1e-2 # 12/6/21: 2.0   # mxy complex error weighting. Seems to need to be non-zero for optimizer to make progress.
     lam3 = 0.0                  # mz error weighting
     lam4 = 0.0                  # flip angle error weighting
 
@@ -66,10 +66,10 @@ def err_ml2xy(Mr_: Tensor, Md_: Tensor, w_: Optional[Tensor] = None) -> Tensor:
 
     Me_ = Mr_[..., :2].norm(dim=-1) - Md_[..., :2].norm(dim=-1)
     #print('Me_.size(): ', Me_.size())
-    #errmag = (Me_ if w_ is None else Me_*w_).norm()**2
+    errmag = (Me_ if w_ is None else Me_*w_).norm()**2
     thresh = 0.2;  # max allowable difference in sin(flip)
     #errmag = (torch.relu((Me_.abs() - thresh)/thresh)).norm()**2
-    errmag = (Me_.abs()).norm()**2
+    #errmag = (Me_.abs()).norm()**2
 
     Me_ = (Mr_[..., :2] - Md_[..., :2])
     errcplx = (Me_ if w_ is None else Me_*w_[..., None]).norm()**2
